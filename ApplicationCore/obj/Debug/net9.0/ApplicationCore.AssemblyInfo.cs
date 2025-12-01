b299eddfb9738f0c1e24cc9432f4598637ee5e52
Anh Do <anhdo.us@gmail.com>
2025-11-30 20:23:17 -0600

    Remove unnecessary file

--- ApplicationCore/obj/Debug/net9.0/ApplicationCore.AssemblyInfo.cs
+++ ApplicationCore/obj/Debug/net9.0/ApplicationCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApplicationCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16e02384c552abad0e408eec582be860f4817d40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1fe2b725a33592fe271387b32b4861769c377c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApplicationCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApplicationCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
